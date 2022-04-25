127459bb01124213f59796aee9322f89c6941a8c
coln clock <jerry@vcampute.com>
2022-04-25 23:17:52 +0800

    basic  commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入样式
+import '@/styles/index.less'
+// 引入vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+// 移动端适配
+import 'amfe-flexible'
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
