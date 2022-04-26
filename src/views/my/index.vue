a156f9095fd4795a2f1c3d0401d0b1655dfbd0f3
coln clock <jerry@vcampute.com>
2022-04-26 23:53:50 +0800

    day

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,13 +1,176 @@
 <template>
-  <div>My</div>
+  <div class="my-container">
+    <!-- 已登录 -->
+    <!-- 已登录头部 -->
+    <div class="header user-info">
+      <div class="base-info">
+        <div class="left">
+          <van-image
+            class="avatar"
+            :src="require('@/assets/longma2.png')"
+            round
+            fit="cover"
+          />
+          <span class="name">越前龙马</span>
+        </div>
+        <div class="right">
+          <van-button size="mini" round to="/user/profile">编辑资料</van-button>
+        </div>
+      </div>
+      <div class="data-stats">
+        <div class="data-item">
+          <span class="count">{{ userInfo.art_count }}</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">{{ userInfo.follow_count }}</span>
+          <span class="text">关注</span>
+        </div>
+        <div class="data-item">
+          <span class="count">{{ userInfo.fans_count }}</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="data-item">
+          <span class="count">{{ userInfo.like_count }}</span>
+          <span class="text">获赞</span>
+        </div>
+      </div>
+    </div>
+    <!-- /已登录头部 -->
+    <!-- 未登录 -->
+    <div class="header not-login">
+      <div class="login-btn" @click="$router.push('/login')">
+        <img class="mobile-img" src="~@/assets/mobile.png" alt="" />
+        <span class="text">登录 / 注册</span>
+      </div>
+    </div>
+
+    <!-- 收藏&历史 -->
+    <van-grid class="grid-nav" :column-num="2" clickable>
+      <van-grid-item class="grid-item">
+        <template #icon>
+          <i class="iconfont icon-shoucang"></i>
+        </template>
+        <template #text> 收藏</template>
+      </van-grid-item>
+      <van-grid-item class="grid-item">
+        <template #icon>
+          <i class="iconfont icon-lishi"></i>
+        </template>
+        <template #text> 收藏</template>
+      </van-grid-item>
+    </van-grid>
+  </div>
 </template>
 
 <script>
 export default {
- name:"My"
-}
+  name: "My",
+  data() {
+    return {
+      userInfo: {},
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.my-container {
+  .header {
+    height: 361px;
+    background: url("~@/assets/banner.png");
+    background-size: cover;
+  }
 
+  .not-login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-btn {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .mobile-img {
+        width: 132px;
+        height: 132px;
+        margin-bottom: 15px;
+      }
+      .text {
+        font-size: 28px;
+        color: #fff;
+      }
+    }
+  }
+
+  .user-info {
+    .base-info {
+      height: 231px;
+      padding: 76px 32px 23px;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .left {
+        display: flex;
+        align-items: center;
+        .avatar {
+          width: 132px;
+          height: 132px;
+          border: 4px solid #fff;
+          margin-right: 23px;
+        }
+        .name {
+          font-size: 30px;
+          color: #fff;
+        }
+      }
+    }
+    .data-stats {
+      display: flex;
+      .data-item {
+        height: 130px;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+        .count {
+          font-size: 36px;
+        }
+        .text {
+          font-size: 23px;
+        }
+      }
+    }
+  }
+
+  .grid-nav {
+    .grid-item {
+      height: 141px;
+      i.iconfont {
+        font-size: 45px;
+      }
+      .icon-shoucang {
+        color: #eb5253;
+      }
+      .icon-lishi {
+        color: #ff9d1d;
+      }
+      span.text {
+        font-size: 28px;
+      }
+    }
+  }
+
+  .logout-cell {
+    text-align: center;
+    color: #d86262;
+  }
+
+  .mb-9 {
+    margin-bottom: 9px;
+  }
+}
 </style>
